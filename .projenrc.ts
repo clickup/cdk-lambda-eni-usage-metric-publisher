5f27d07b7654e5003574e04a41ac60591227682e
jose-clickup <jamoroso@clickup.com>
2023-11-08 23:37:49 -0500

    chore: run projen build

--- .projenrc.ts
+++ .projenrc.ts
@@ -1,4 +1,7 @@
 import { clickupCdk } from '@time-loop/clickup-projen';
+import { JsonPatch } from 'projen';
+
+const repoName = 'cdk-lambda-eni-usage-metric-publisher';
 const project = new clickupCdk.ClickUpCdkConstructLibrary({
   author: 'jose-clickup',
   authorAddress: 'jamoroso@clickup.com',
@@ -17,4 +20,18 @@ const project = new clickupCdk.ClickUpCdkConstructLibrary({
   // description: undefined,  /* The description is just a string that helps people understand the purpose of the package. */
   // packageName: undefined,  /* The "name" in package.json. */
 });
+// Assume the usInfraDev role
+const build = project.tryFindObjectFile('.github/workflows/build.yml');
+build?.addOverride('jobs.build.permissions', { 'id-token': 'write' });
+build?.patch(
+  JsonPatch.add('/jobs/build/steps/0', {
+    name: 'Configure AWS Credentials',
+    uses: 'aws-actions/configure-aws-credentials@v2',
+    with: {
+      'aws-region': 'us-west-2',
+      'role-to-assume': `arn:aws:iam::425845004253:role/${repoName}-github-actions-role`,
+      'role-duration-seconds': 900,
+    },
+  }),
+);
 project.synth();
